bfd8f65b006588db4340b574b544bc11c1767c9f
Thomas Brace <tommy-brace-22@hotmail.com>
2019-12-22 21:55:41 +0000

    Add linkedin link, increase padding

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ let skills = document.querySelectorAll(".skill");
 
 for (let skill of skills) {
     skill.addEventListener("click", function() {
+        let headerImage = this.querySelector(".skill-header img");
+
         if (this.classList.contains("close")) {
             this.classList.add("open");
             this.classList.remove("close");
+
+            headerImage.setAttribute("src", headerImage.getAttribute("minus-src"));
         } else {
             this.classList.add("close");
             this.classList.remove("open");
+
+            headerImage.setAttribute("src", headerImage.getAttribute("plus-src"));
         }
     });
 }
