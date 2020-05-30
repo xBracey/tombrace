fd20e4599e730edd077777a605a5bb36e593ec97
Tom Brace <tommy-brace-22@hotmail.com>
2020-05-30 15:46:20 +0100

    Add projects page

---
+++ src/pages/projects.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Layout, Box } from "../components";
+import { Heading } from "./index.styled";
+
+const Projects = ({ data, path }: any) => (
+    <Layout title="Projects">
+        <Box width={"calc(100% - 80px)"}>
+            <Heading>Projects</Heading>
+        </Box>
+    </Layout>
+);
+
+export default Projects;
